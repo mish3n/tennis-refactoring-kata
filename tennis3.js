dd8b6b857109d43d5ee9d61db70c4d7d1a09695d
mish3n <mish3n@gmail.com>
2021-02-26 18:48:10 +0200

    T3 separate logic depending on score equality

--- tennis3.js
+++ tennis3.js
@@ -4,6 +4,23 @@ function getScore(player1Score, player2Score) {
     var score;
     let player1Name = "player1";
     let player2Name = "player2";
+
+    if(player1Score === player2Score) {
+        if ((player1Score < 4 && player2Score < 4) && (player1Score + player2Score < 6)) {
+            var points = ["Love", "Fifteen", "Thirty", "Forty"];
+            if(player1Score === player2Score) {
+                return points[player1Score] + "-All";
+            }
+            return  points[player1Score] + "-" + points[player2Score];
+        } else {
+            if (player1Score === player2Score) {
+                return "Deuce";
+            }
+            score = player1Score > player2Score ? player1Name : player2Name;
+            return ((player1Score - player2Score) * (player1Score - player2Score) === 1) ? "Advantage " + score : "Win for " + score;
+        }
+    }
+
     if ((player1Score < 4 && player2Score < 4) && (player1Score + player2Score < 6)) {
         var points = ["Love", "Fifteen", "Thirty", "Forty"];
         if(player1Score === player2Score) {
