9e9b4fbd92bbe5c5216bf75ddd83bb8671102a25
mish3n <mish3n@gmail.com>
2021-02-26 17:35:51 +0200

    T1 refactor

--- tennis1.js
+++ tennis1.js
@@ -17,21 +17,29 @@ function getPlayerInLead(player1_score, player2_score) {
     return player1_score > player2_score ? "player1" : "player2";
 }
 
-function getScore(player1_score, player2_score) {
-    if (player1_score === player2_score) {
-        return getTiedScore(player1_score);
-    } else if (player1_score >= 4 || player2_score >= 4) {
-        var scoreDifference = Math.abs(player1_score - player2_score);
-        let playerInLead = getPlayerInLead(player1_score, player2_score);
+function isAnyPlayerAbove3pt(player1_score, player2_score) {
+    return player1_score >= 4 || player2_score >= 4;
+}
 
-        if (scoreDifference === 1) {
-            return `Advantage ${playerInLead}`;
-        }  
-        
-        return `Win for ${playerInLead}`;
-    } else {
-        return `${getScoreName(player1_score)}-${getScoreName(player2_score)}`;
+function getWinOrAdvantageScore(scoreDifference, playerInLead) {
+    if (scoreDifference === 1) {
+        return `Advantage ${playerInLead}`;
     }
+    
+    return `Win for ${playerInLead}`;
+}
+
+function getScore(player1_score, player2_score) {
+    let scoreDifference = Math.abs(player1_score - player2_score);
+
+    if (scoreDifference === 0) {
+        return getTiedScore(player1_score);
+    } else if (isAnyPlayerAbove3pt(player1_score, player2_score)) {
+        let playerInLead = getPlayerInLead(player1_score, player2_score);
+        return getWinOrAdvantageScore(scoreDifference, playerInLead);
+    }
+    
+    return `${getScoreName(player1_score)}-${getScoreName(player2_score)}`;
 }
 
 module.exports = getScore;
