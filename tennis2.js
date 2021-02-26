957b52f4d6457467974f6012a77205ca94637986
mish3n <mish3n@gmail.com>
2021-02-26 18:11:56 +0200

    T2 reorganize and delete unused var

--- tennis2.js
+++ tennis2.js
@@ -20,37 +20,31 @@ function getPlayerInLead(player1Points, player2Points) {
 }
 
 function getScore(player1Points, player2Points) {
-    var score = "";
-
     if (player1Points === player2Points && player1Points < 3) {
-        score = getScoreName(player1Points);
-        score += "-All";
+        return getScoreName(player1Points) + "-All";
     }
 
     if (player1Points === player2Points && player1Points > 2) {
-        score = "Deuce";
+        return "Deuce";
     }
 
     let playerInLead = getPlayerInLead(player1Points, player2Points);
+    if ((player1Points >= 4 && player2Points >= 0 && (player1Points - player2Points) >= 2) || 
+        (player2Points >= 4 && player1Points >= 0 && (player2Points - player1Points) >= 2)) {
+        return "Win for " + playerInLead;
+    }
 
     if ((player1Points > player2Points && player2Points >= 3) || 
         (player2Points > player1Points && player1Points >= 3)) {
-        score = "Advantage " + playerInLead;
-    }
-
-    if ((player1Points >= 4 && player2Points >= 0 && (player1Points - player2Points) >= 2) || 
-        (player2Points >= 4 && player1Points >= 0 && (player2Points - player1Points) >= 2)) {
-        score = "Win for " + playerInLead;
+        return "Advantage " + playerInLead;
     }
 
     if ((player1Points > 0 && player2Points === 0) || (player2Points > 0 && player1Points === 0) || 
         (player1Points > player2Points && player1Points < 4) || (player2Points > player1Points && player2Points < 4)) {
         let P1res = getScoreName(player1Points);
         let P2res = getScoreName(player2Points);
-        if(!score.length) score = P1res + "-" + P2res;
+        return P1res + "-" + P2res;
     }
-
-    return score;
 }
 
 
